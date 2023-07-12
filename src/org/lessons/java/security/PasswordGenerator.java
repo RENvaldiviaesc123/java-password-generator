9b893e325fad27d96793cd1a31b5e927b207dee5
renat <guitarra123primera@gmail.com>
2023-07-12 14:35:02 +0200

    Page Setup!

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,14 @@
+package org.lessons.java.security;
+/*
+    _Il programma deve salvare in opportuni variabili il nome, cognome, colore
+    preferito e data di nascita di un utente suddivisa in giorno, mese e anno (in numero)
+    _Generare (e stampare a video) una password concatenando nome, cognome, colore preferito
+    e somma di giorno, mese e anno di nascita, separate dal carattere "-".
+    ESEMPIO:Pinco-Pallo-magenta-2011
+ */
+public class PasswordGenerator {
+    public static void main(String[] args) {
+    //Dichiariamo le variabili
+
+    }
+}
